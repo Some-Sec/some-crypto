7368cb809a0fa6fd7ffbb061a6688314885d8560
Thomas Schalldach <tom@riddle.wtf>
2022-11-05 21:14:53 +0100

    Adding decryption operation and test

--- src/main/java/com/somesec/crypto/decrypt/AESDecryption.java
+++ src/main/java/com/somesec/crypto/decrypt/AESDecryption.java
@@ -10,6 +10,8 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.GCMParameterSpec;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.security.Key;
 
@@ -22,6 +24,7 @@ public final class AESDecryption implements DecryptionOperation {
 
     /**
      * Creates a new {@link AESDecryption} instance with the passed {@link ConfigurationResolver}
+     *
      * @param resolver
      */
     public AESDecryption(final ConfigurationResolver resolver) {
@@ -39,6 +42,25 @@ public final class AESDecryption implements DecryptionOperation {
             bb.get(cipherText);
             cipher.init(Cipher.DECRYPT_MODE, key, new GCMParameterSpec((int) resolver.getConfig(DefaultConfig.AES_GCM_TAG_LENGTH_BYTE) * (int) resolver.getConfig(DefaultConfig.BIT_IN_A_BYTE), iv));
             return cipher.doFinal(cipherText);
+        } catch (Exception ex) {
+            throw new CryptoOperationException(MessagesCode.ERROR_DECRYPTION_ALGO, ex, getAlgorithmName());
+        }
+    }
+
+    @Override
+    public void decrypt(InputStream cipherText, OutputStream plainText, Key key) {
+        try (cipherText; plainText) {
+            Cipher cipher = Cipher.getInstance(resolver.getConfig(DefaultConfig.AES_CIPHER_NAME), BouncyCastleProvider.PROVIDER_NAME);
+            final int nonceLength = resolver.getConfig(DefaultConfig.AES_GCM_NONCE_LENGTH);
+            if (cipherText.available() < nonceLength) {
+                throw new CryptoOperationException(MessagesCode.ERROR_DECRYPTION_ALGO,getAlgorithmName());
+            }
+            byte[] iv= cipherText.readNBytes(nonceLength);
+            cipher.init(Cipher.DECRYPT_MODE, key, new GCMParameterSpec((int) resolver.getConfig(DefaultConfig.AES_GCM_TAG_LENGTH_BYTE) * (int) resolver.getConfig(DefaultConfig.BIT_IN_A_BYTE), iv));
+            while (cipherText.available()>0){
+                plainText.write(cipher.update(cipherText.readNBytes(resolver.getConfig(DefaultConfig.MEGA_BYTE))));
+            }
+            plainText.write(cipher.doFinal());
         } catch (Exception ex) {
             throw new CryptoOperationException(MessagesCode.ERROR_DECRYPTION_ALGO, ex, getAlgorithmName());
         }
--- src/main/java/com/somesec/crypto/decrypt/DecryptionService.java
+++ src/main/java/com/somesec/crypto/decrypt/DecryptionService.java
@@ -1,5 +1,7 @@
 package com.somesec.crypto.decrypt;
 
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.security.Key;
 
 /**
@@ -26,5 +28,16 @@ public interface DecryptionService {
      * @return the decrypted bytes
      */
     byte[] decrypt(byte[] cypheredBytes, Key key);
+
+    /**
+     * Decrypts the cipherText received from the InputStream and writes it into the OutputStream
+     * This operation is recommended for large files as it should be, depending on the stream implementation, less memory intensive.
+     *
+     * It is expected that the streams will be closed once this method has finished
+     * @param cipherText the data stream delivering the data to be decrypted
+     * @param plainText the stream delivering the plainText
+     * @param key     the encryption key, either symmetric or asymmetric
+     */
+    void decrypt(InputStream cipherText, OutputStream plainText, Key key);
 
 }
